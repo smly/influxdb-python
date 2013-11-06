58e749572b8e284dc382ad19ab7214a2e33f10cb
Kohei Ozaki <eowner@gmail.com>
2013-11-07 03:44:47 +0900

    Update tests to check Exception

--- tests/influxdb/client_test.py
+++ tests/influxdb/client_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests
-import nose.tools
+from nose.tools import raises
 from mock import patch
 
 from influxdb import InfluxDBClient
@@ -8,8 +8,8 @@ from influxdb import InfluxDBClient
 
 def _build_response_object(status_code=200, content=""):
     resp = requests.Response()
-    resp.status_code = 200
-    resp.content = content
+    resp.status_code = status_code
+    resp._content = content
     return resp
 
 
@@ -24,3 +24,31 @@ class TestInfluxDBClient(object):
         cli.switch_username('another_username', 'another_password')
         assert cli._username == 'another_username'
         assert cli._password == 'another_password'
+
+    def test_create_database(self):
+        with patch.object(requests, 'post') as mocked_post:
+            mocked_post.return_value = _build_response_object()
+            cli = InfluxDBClient('host', 8086, 'username', 'password', 'db')
+            assert cli.create_database('new_db') is True
+
+    @raises(Exception)
+    def test_creata_database_fails(self):
+        with patch.object(requests, 'post') as mocked_post:
+            mocked_post.return_value = _build_response_object(status_code=401)
+            cli = InfluxDBClient('host', 8086, 'username', 'password', 'db')
+            cli.create_database('new_db')
+
+    def test_delete_database(self):
+        with patch.object(requests, 'delete') as mocked_post:
+            mocked_post.return_value = _build_response_object()
+            cli = InfluxDBClient('host', 8086, 'username', 'password', 'db')
+            assert cli.delete_database('old_db') is True
+
+    @raises(Exception)
+    def test_delete_database_fails(self):
+        with patch.object(requests, 'delete') as mocked_post:
+            mocked_post.return_value = _build_response_object(status_code=401)
+            cli = InfluxDBClient('host', 8086, 'username', 'password', 'db')
+            cli.delete_database('old_db')
+
+
